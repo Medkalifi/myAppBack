b1d07a9e9689ba31bde2ded68b0f7f90839d8de3
Melkhalifi <melkhalifi2@gamil.com>
2021-08-27 16:37:00 +0200

    add file package

--- src/main/java/fr/moha/myApp/files/controller/FileResource.java
+++ src/main/java/fr/moha/myApp/files/controller/FileResource.java
@@ -1,5 +1,10 @@
 package fr.moha.myApp.files.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
 public class FileResource {
 
 }
