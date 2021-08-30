3b0389ec827456c7697b28642ff266a699f0133c
Melkhalifi <melkhalifi2@gamil.com>
2021-08-30 14:31:31 +0200

    experience pro

--- src/main/java/fr/moha/myApp/files/controller/FileResourceController.java
+++ src/main/java/fr/moha/myApp/files/controller/FileResourceController.java
@@ -28,54 +28,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/file")
 public class FileResourceController {
 
 	// define a location
-    public static final String DIRECTORY = System.getProperty("user.home") + "/fichiers/";
+	public static final String DIRECTORY = System.getProperty("user.home") + "/fichiers/";
 
-    // Define a method to upload files
-    @PostMapping("/upload")
-    public ResponseEntity<List<String>> uploadFiles(@RequestParam("files")List<MultipartFile> multipartFiles) throws IOException {
-        List<String> filenames = new ArrayList<>();
-        for(MultipartFile file : multipartFiles) {
-            String filename = StringUtils.cleanPath(file.getOriginalFilename());
-            Path fileStorage = get(DIRECTORY+"uploads", filename).toAbsolutePath().normalize();
-            System.out.println("le nom du fichier est "+ filename);
-            copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
-            
-            System.out.println("le path du fichier est "+ fileStorage);
-            filenames.add(filename);
-        }
-        return ResponseEntity.ok().body(filenames);
-    }
-    
-    // Define a method to download files
-    @GetMapping("download/{filename}")
-public ResponseEntity<Resource> downloadFiles(@PathVariable("filename")String filename) throws Exception{
-	Path filePath = get(DIRECTORY+"uploads").toAbsolutePath().normalize().resolve(filename);
-	System.out.println("le path est "+filePath);
-	
-	 filename = filePath.getFileName().toString();
-	 System.out.println("le nom du fichier est "+filename);
-	if(!Files.exists(filePath)) {
-        throw new FileNotFoundException(filename + " n'a pas été trouvé dans le serveur ...");
+	// Define a method to upload files
+	@PostMapping("/upload")
+	public ResponseEntity<List<String>> uploadFiles(@RequestParam("files") List<MultipartFile> multipartFiles)
+			throws IOException {
+		List<String> filenames = new ArrayList<>();
+		for (MultipartFile file : multipartFiles) {
+			String filename = StringUtils.cleanPath(file.getOriginalFilename());
+			Path fileStorage = get(DIRECTORY + "uploads", filename).toAbsolutePath().normalize();
+			System.out.println("le nom du fichier est " + filename);
+			copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
+
+			System.out.println("le path du fichier est " + fileStorage);
+			filenames.add(filename);
+		}
+		return ResponseEntity.ok().body(filenames);
 	}
-        Resource resource = new UrlResource(filePath.toUri());
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("File-Name", filePath.getFileName().toString());
-       
-       
-        
-        httpHeaders.add(CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename().toString());
+
+	// Define a method to download files
+	@GetMapping("download/{filename}")
+	public ResponseEntity<Resource> downloadFiles(@PathVariable("filename") String filename) throws Exception {
+		Path filePath = get(DIRECTORY + "uploads").toAbsolutePath().normalize().resolve(filename);
+		
+
+		
+		
+		if (!Files.exists(filePath)) {
+			throw new FileNotFoundException(filename + " n'a pas été trouvé dans le serveur ...");
+		}
+		Resource resource = new UrlResource(filePath.toUri());
+		HttpHeaders httpHeaders = new HttpHeaders();
+		//httpHeaders.add("File-Name", filePath.getFileName().toString());
+
+		httpHeaders.add(CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename());
         return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
                 .headers(httpHeaders).body(resource);
-        
-    }
-}
-	
-	
 
+	}
+}
